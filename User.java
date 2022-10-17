168c16ee7b6eb5ed3687e4c71c62e2c0c9d31d04
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2022-10-17 02:06:56 -0400

    User Class Stubs

---
+++ User.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/**
+ * @author Katelyn Wyandt
+ * Description: The user class contains the variables for all classes that inherit user, includng id,
+ * email, phone, password, name, birthday, and security question
+ */
+
+ public abstract class User {
+    protected UUID id;
+    protected String email;
+    protected String phone;
+    protected String password;
+    protected String firstName;
+    protected String lastName;
+    protected Date birthDate;
+    protected Map <String, String> securityQuestions;
+
+    /**
+     * Constructor for the User class to create a new user
+     * @param email email address of the user
+     * @param pass password for login to the web console, specific for each user
+     * @param first first name of user
+     * @param last last name of user
+     * @param phone phone number of user
+     * @param birthDate birthday of user
+     * @param question security question for user login
+     */
+    public User (String email, String pass, String first, String last, String phone, Date birthDate, Map<String, String> question){
+
+    }
+
+    /**
+     * Abstract method for user to access and edit login information
+     */
+    public abstract void editInformation();
+
+    /**
+     * Abstract method for user to submit information for login
+     */
+    public abstract void submitInformation();
+
+    /**
+     * Method for user to change login password
+     */
+    public void changePassword() {
+
+    }
+
+ }
