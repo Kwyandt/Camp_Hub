cbeb0d73b128f9fed5cee4ea042bf683f762b4c8
NathanBickel <nbickel@email.sc.edu>
2022-10-16 21:10:01 -0400

    Added DataConstants

---
+++ DataConstants.java
@@ -0,0 +1,64 @@
+/**
+ * DataConstants class for saving/loading data
+ * @author Nathan Bickel
+ */
+public abstract class DataConstants {
+	protected static final String CAMP_NAME = "name";
+	protected static final String CAMP_SESSIONS = "sessions";
+	protected static final String CAMP_FAQS = "faqs";
+	protected static final String CAMP_ACTIVITIES = "activities";
+	protected static final String CAMP_OFFICE_PHONE = "officePhone";
+	protected static final String CAMP_PACKING_LIST = "packingList";
+	protected static final String ACTIVITY_ID = "id";
+	protected static final String ACTIVITY_NAME = "name";
+	protected static final String ACTIVITY_DESCRIPTION = "description";
+	protected static final String ACTIVITY_LOCATION = "location";
+	protected static final String SESSION_ID = "id";
+	protected static final String SESSION_PRICE = "price";
+	protected static final String SESSION_THEME = "theme";
+	protected static final String SESSION_CABINS = "cabins";
+	protected static final String SESSION_PRIORITY_DEADLINE = "priorityDeadline";
+	protected static final String SESSION_REGULAR_DEADLINE = "regularDeadline";
+	protected static final String SESSION_START_DATE = "startDate";
+	protected static final String SESSION_END_DATE = "endDate";
+	protected static final String CABIN_ID = "id";
+	protected static final String CABIN_SCHEDULE = "schedule";
+	protected static final String CABIN_COUNSELOR = "counselor";
+	protected static final String SESSION_CAMPERS = "campers";
+	protected static final String SCHEDULE_ID = "id";
+	protected static final String SCHEDULE_ACTIVITIES = "activities";
+	protected static final String USER_ID = "id";
+	protected static final String USER_EMAIL = "email";
+	protected static final String USER_PHONE = "phone";
+	protected static final String USER_PASSWORD = "password";
+	protected static final String USER_FIRST_NAME = "firstName";
+	protected static final String USER_LAST_NAME = "lastName";
+	protected static final String USER_BIRTH_DATE = "birthDate";
+	protected static final String USER_SECURITY_QUESTIONS = "securityQuestions";
+	protected static final String DIRECTOR_BIO = "bio";
+	protected static final String DIRECTOR_NOTES = "notes";
+	protected static final String PARENT_CHILDREN = "children";
+	protected static final String PARENT_DISCOUNT = "discount";
+	protected static final String PARENT_IS_RETURNING = "isReturning";
+	protected static final String COUNSELOR_MEDS = "meds";
+	protected static final String COUNSELOR_ALLERGIES = "allergies";
+	protected static final String COUNSELOR_EMERGENCY_CONTACTS = "emergencyContacts";
+	protected static final String COUNSELOR_DIETARY_RESTRICTIONS = "dietaryRestrictions";
+	protected static final String COUNSELOR_T_SHIRT = "tShirt";
+	protected static final String COUNSELOR_BIO = "bio";
+	protected static final String COUNSELOR_NOTES = "notes";
+	protected static final String CAMPER_ID = "id";
+	protected static final String CAMPER_FIRST_NAME = "firstName";
+	protected static final String CAMPER_LAST_NAME = "lastName";
+	protected static final String CAMPER_BIRTH_DATE = "birthDate";
+	protected static final String CAMPER_MEDS = "meds";
+	protected static final String CAMPER_ALLERGIES = "allergies";
+	protected static final String CAMPER_EMERGENCY_CONTACTS = "emergencyContacts";
+	protected static final String CAMPER_DIETARY_RESTRICTIONS = "dietaryRestrictions";
+	protected static final String CAMPER_T_SHIRT = "tShirt";
+	protected static final String EMERGENCY_CONTACT_ID = "id";
+	protected static final String EMERGENCY_CONTACT_FIRST_NAME = "firstName";
+	protected static final String EMERGENCY_CONTACT_LAST_NAME = "lastName";
+	protected static final String EMERGENCY_CONTACT_PHONE_NUMBER = "phoneNumber";
+}
+
