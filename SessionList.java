b29ad7c308f0ef3c0439c5388c626eb6879ead26
lunarm1st <89537597+lunarm1st@users.noreply.github.com>
2022-10-18 20:41:12 -0400

    Added javadoc comments

--- SessionList.java
+++ SessionList.java
@@ -6,18 +6,43 @@ public class SessionList {
     private ArrayList<String> Session;
     private SessionList SessionList;
 
+    
+    /** 
+     * Private constructor for singleton.
+     * @return SessionList
+     */
     private SessionList SessionList() {
         return null;
     }
 
+    
+    /** 
+     * Creates an instance of the class.
+     * @return SessionList
+     */
     public SessionList getInstance() {
         return null;
     }
 
+    
+    /** 
+     * Adds a session with the given parameters.
+     * @param theme
+     * @param priorityDeadline
+     * @param regularDeadline
+     * @param start
+     * @param end
+     */
     public void addSession(String theme, Date priorityDeadline, Date regularDeadline, Date start, Date end) {
 
     }
 
+    
+    /** 
+     * Gets the session based on start.
+     * @param start
+     * @return Session
+     */
     public Session getSession(Date start) {
         return null;
     }
