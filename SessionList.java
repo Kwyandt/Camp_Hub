9a266b97532ee5589726fa92cf40f1d762111cc5
lunarm1st <89537597+lunarm1st@users.noreply.github.com>
2022-10-18 20:38:05 -0400

    SessionList and UserList stubs

---
+++ SessionList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SessionList {
+    
+    private ArrayList<String> Session;
+    private SessionList SessionList;
+
+    private SessionList SessionList() {
+        return null;
+    }
+
+    public SessionList getInstance() {
+        return null;
+    }
+
+    public void addSession(String theme, Date priorityDeadline, Date regularDeadline, Date start, Date end) {
+
+    }
+
+    public Session getSession(Date start) {
+        return null;
+    }
+
+    public void editSession() {
+
+    }
+
+    public void saveSessions() {
+
+    }
+
+}
---
+++ Users/UserList.java
@@ -0,0 +1,36 @@
+package Users;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+public class UserList {
+    
+    private ArrayList<User> users;
+    private UserList userList;
+
+    private UserList UserList() {
+        return null;
+    }
+
+    public UserList getInstance() {
+        return null;
+    }
+
+    public void addUser(String email, String phone, String password, String firstName, String lastName, Date birthdate, Map<String, String> securityQuestion) {
+
+    }
+
+    public User getUser(String email) {
+        return null;
+    }
+
+    public void editUser() {
+
+    }
+
+    public void saveUsers() {
+
+    }
+
+}
