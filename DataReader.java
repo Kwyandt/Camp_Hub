86dc1c5d090f072bb4e65d011917aa8767eb3fc1
NathanBickel <nbickel@email.sc.edu>
2022-10-19 21:42:17 -0400

    Created DataReader stub

---
+++ DataReader.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import Users.*;
+
+/**
+ * @author Nathan Bickel
+ * Class that reads in data from JSON files
+ */
+public class DataReader extends DataConstants {
+
+    /**
+     * Reads in user data and populates list of users
+     * @return ArrayList containing each user
+     */
+    public static ArrayList<User> getAllUsers() {
+        return null;
+    }
+
+    /**
+     * Reads in camp data and returns camp object
+     * @return Camp object with associated data
+     */
+    public static Camp getCamp() {
+        return null;
+    }
+
+}
