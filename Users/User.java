065d65ee794166dfe7327bd59a5419c1862fea2a
jrginn <jrginn47@gmail.com>
2022-10-28 12:54:09 -0400

    added User mutators

--- Users/User.java
+++ Users/User.java
@@ -60,12 +60,24 @@ import java.util.*;
       return email;
     }
 
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
     public String getPhone() {
       return phone;
     }
 
+    public void setPhone(String phone) {
+      this.phone = phone;
+    }
+
     public String getPassword() {
       return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
     }
 
     public String getFirstName() {
