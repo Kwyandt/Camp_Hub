9ef57dfc47881d33efe0d2aede9f21ded5a6b6f2
jrginn <jrginn47@gmail.com>
2022-10-28 17:21:50 -0400

    added viewing methods to Schedule

--- Schedule.java
+++ Schedule.java
@@ -1,4 +1,7 @@
 import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.HashMap;
@@ -6,6 +9,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -68,16 +72,38 @@ public class Schedule {
         return "Activities: " + this.activities.toString();
     }
 
-    public String displayOrderedSchedule() {
+    /**
+     * Creates a String visualizing the cabin's schedule
+     * @param sessionNumber session of specific cabin
+     * @param cabinNumber cabin number
+     * @return String representation of schedule
+     */
+    public String displayOrderedSchedule(int sessionNumber, int cabinNumber) {
         String str = "";
+        str += "Schedule for Session " + sessionNumber + ", Cabin " + cabinNumber + "\n\n\n";
+        // array tracking if day has been printed yet
+        boolean[] days = new boolean[7];
+        String dashes = "---------------------";
         for(Date d: activities.keySet()) {
-            str += d.toString() + " - " + activities.get(d).getName() + " - " + activities.get(d).getLocation() + "\n";
+            Instant i = d.toInstant();
+            LocalDateTime ldt = LocalDateTime.ofInstant(i, ZoneId.of("EST"));
+            // day has not been printed yet
+            if(!days[dayToNumber(ldt.getDayOfWeek())]) {
+                str += dashes + ldt.getDayOfWeek() + ", " + ldt.getMonth() + ". " + ldt.getDayOfMonth() + dashes;
+                days[dayToNumber(ldt.getDayOfWeek())] = true;
+            }
+            str += ldt.getHour() + ":" + ldt.getMinute() + " - " + activities.get(d).getName() + " at " + activities.get(d).getLocation() + "\n";
         }
         return str;
     }
 
+    /**
+     * Saves schedule to a txt file
+     * @param sessionNumber session of specific cabin
+     * @param cabinNumber cabin number
+     */
     public void printOrderedSchedule(int sessionNumber, int cabinNumber) {
-        String str = this.displayOrderedSchedule();
+        String str = this.displayOrderedSchedule(sessionNumber, cabinNumber);
         String fileName = "session" + sessionNumber + "cabin" + cabinNumber + "schedule.txt";
         try(FileWriter file = new FileWriter(fileName)) {
             file.write(str);
@@ -94,5 +120,25 @@ public class Schedule {
      */
     public boolean equals(Schedule otherSchedule) {
         return activities.equals(otherSchedule.getActivities());
+    }
+
+    private int dayToNumber(DayOfWeek day) {
+        switch(day) {
+            case SUNDAY:
+                return 0;
+            case MONDAY:
+                return 1;
+            case TUESDAY:
+                return 2;
+            case WEDNESDAY:
+                return 3;
+            case THURSDAY:
+                return 4;
+            case FRIDAY:
+                return 5;
+            case SATURDAY:
+                return 6;
+        }
+        return -1;
     }
 }
