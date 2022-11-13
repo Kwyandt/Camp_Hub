af760a2875ad3b25762c22835a3fa049946dde05
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2022-11-13 13:10:02 -0500

    stubs and stuff

--- DataReaderTest.java
+++ DataReaderTest.java
@@ -3,6 +3,74 @@
  * 
  * Brief: Unit testing of project, data reader testing
  */
-public class DataReaderTest {
-    
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+
+import Users.Director;
+import Users.Parent;
+import Users.User;
+
+public class DataReaderTest{
+    private UserList userList = UserList.getInstance();
+    private static ArrayList<User> newUsers = new ArrayList<User>();
+    Map<String, String> questions = new HashMap<>();
+    private Camp camp = Camp.getInstance();
+
+
+    @BeforeEach
+    public void setup () {
+        newUsers.clear();
+        questions.put("question", "answer");
+        questions.put("question2", "answer2");
+        newUsers.add(new Director("samsamuels@camptallrock.com", "ilovecamps", "Samantha", "Samuels", "(304) 474-4938", new Date(),questions));
+        newUsers.add(new Parent ("email@email.com", "password", "John", "Smith", "123-456-7890", new Date(9-12-2000),questions));
+        DataWriter.saveAllUsers(newUsers);
+        DataWriter.saveCamp(camp);
+    }
+
+    @AfterEach
+    public void tearDown () {
+        UserList.getInstance().getAllUsers().clear();
+        DataWriter.saveAllUsers(newUsers);
+        DataWriter.saveCamp(camp);
+    }
+
+    @Test
+    void testReadUserSize() {
+
+    }
+
+    @Test 
+    void testReadUserZero() {
+
+    }
+
+    @Test 
+    void testReadFirstName(){
+
+    }
+
+    @Test 
+    void testReadFirstNameNull(){
+
+    }
+
+    @Test 
+    void testGetCamp() {
+
+    }
+
+    @Test
+    void testGetCampActivities() {
+
+    }
+
+    @Test 
+    void testGetNullActivities() {
+
+    }
 }
--- DataWriterTest.java
+++ DataWriterTest.java
@@ -3,6 +3,102 @@
  * 
  * Brief: Unit Testing of project, Data Writer Testing
  */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.*;
+
+import Users.User;
 public class DataWriterTest {
-    
+    //test users, camp, cabin
+    private UserList userList = UserList.getInstance();
+    private static ArrayList<User> newUsers = new ArrayList<User>();
+    private Camp camp = Camp.getInstance();
+
+    @BeforeEach
+    public void setup () {
+        UserList.getInstance().getAllUsers().clear();
+        DataWriter.saveAllUsers((newUsers));
+        DataWriter.saveCamp(camp);
+    }
+
+    @AfterEach
+    public void teadDown () {
+        UserList.getInstance().getAllUsers().clear();
+        DataWriter.saveAllUsers((newUsers));
+        DataWriter.saveCamp(camp);
+    }
+
+    @Test
+    void testWriteZero() {
+
+    }
+
+    @Test 
+    void testWriteOne() {
+
+    }
+
+    @Test
+    void testWriteFive() {
+
+    }
+
+    @Test
+    void testWriteEmpty() {
+
+    }
+
+    @Test 
+    void testWriteNull () {
+
+    }
+
+    @Test 
+    void testWriteCabinRoster() {
+
+    }
+
+    @Test 
+    void testWriteEmptyRoster() {
+
+    }
+
+    @Test
+    void testWriteNullRoster() {
+
+    }
+
+    @Test 
+    void testWriteCabinVitals() {
+
+    }
+
+    @Test 
+    void testWriteEmptyVitals() {
+
+    }
+
+    @Test 
+    void testWriteNullVitals() {
+
+    }
+
+    @Test 
+    void testWriteCabinSchedule() {
+
+    }
+
+    @Test 
+    void testWriteEmptySchedule() {
+
+    }
+
+    @Test 
+    void testWriteNullSchedule() {
+
+    }
+
 }
