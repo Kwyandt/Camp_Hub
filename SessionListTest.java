ef4c495bd13bc63a8ab6bf97e692c93a54c555f2
jrginn <jrginn47@gmail.com>
2022-11-13 15:01:44 -0500

    further SessionList testing

--- SessionListTest.java
+++ SessionListTest.java
@@ -17,6 +17,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Users.*;
+
 public class SessionListTest {
 
     private SessionList  sessionList = SessionList.getInstance();
@@ -53,6 +55,40 @@ public class SessionListTest {
         } catch (Exception e) {
             return null;
         } 
+    }
+
+    private static Camper[] createKBasicCampers(int k) {
+        Camper[] camperList = new Camper[k];
+        for (int i = 0; i < k; i++) {
+            Map<Relationship,EmergencyContact> emergencyContacts = new HashMap<Relationship,EmergencyContact>();
+            emergencyContacts.put(Relationship.DENTIST, new EmergencyContact("Dentist", 
+                                                                             Integer.toString(k), 
+                                                                             "(555) 555-5555", 
+                                                                             "123 Heh Dr"));
+            emergencyContacts.put(Relationship.DOCTOR, new EmergencyContact("Doctor", 
+                                                                             Integer.toString(k), 
+                                                                             "(555) 555-5555", 
+                                                                             "123 Heh Dr"));
+            emergencyContacts.put(Relationship.GUARDIAN, new EmergencyContact("Guardian", 
+                                                                             Integer.toString(k), 
+                                                                             "(555) 555-5555", 
+                                                                             "123 Heh Dr"));                                                                
+            camperList[i] = new Camper("Camper",
+                                      Integer.toString(k),
+                                      getDate("01-Jan-2010"),
+                                      emergencyContacts,
+                                      "S");
+        }
+        return camperList;
+            
+    }
+
+    public static Counselor createBasicCounselor() {
+        Map<String, String> securityQuestion = new HashMap<String, String>();
+        securityQuestion.put("What was the name of your elementary school?", "Martinez Elementary School");
+        securityQuestion.put("What was the make of your first car?", "1995 White Ford Ranger");
+        Counselor c = new Counselor("test@gmail.com", "kj;ladfss", "Coun", "Selor", "5555555555", getDate("01-Jan-2003"), securityQuestion);
+        return c;
     }
 
     @Test
@@ -118,5 +154,161 @@ public class SessionListTest {
             }
         }
         assertTrue(count <= 1, "Session objects should not be added twice");
+    }
+
+    @Test
+    public void getSessionByValidDate() {
+        assertTrue(sessionList.getSession(getDate("07-Jul-2023")) != null, "Can access session through start date");
+    }
+
+    @Test
+    public void invalidStartDateGetSession() {
+        assertTrue(sessionList.getSession(getDate("10-Jul-2003")) == null, "If no session has this start date it returns null");
+    }
+
+    @Test
+    public void nullStartDateGetSession() {
+        assertTrue(sessionList.getSession(getDate(null)) == null, "Null start date should not access a session");
+    }
+
+    @Test 
+    public void getSessionByValidIndex() {
+        sessionList.addSession(s);
+        assertTrue(sessionList.getSession(0) != null, "A session should be in SessionList");
+    }
+
+    @Test
+    public void getSessionByInvalidIndex() {
+        try {
+            assertTrue(sessionList.getSession(-1) == null, "This should not return a session");
+        }
+        catch(IndexOutOfBoundsException e) {
+            fail("Unhandled index out of bounds exception");
+        }
+    }
+
+    @Test
+    public void editExistingSession() {
+        Session edited = sessionList.getSession(0);
+        edited.setDescription("New description");
+        Date start = edited.getStartDate();
+        sessionList.editSession(edited);
+        assertTrue(sessionList.getSession(start).getDescription().equals("New description"), "Edit was successful");
+    }
+
+    @Test
+    public void editAbsentSession() {
+        assertFalse(sessionList.editSession(s), "Should return false if session is not added");
+    }
+
+    @Test
+    public void editNullSession() {
+        try {
+            assertFalse(sessionList.editSession(null), "Should return false if trying to edit null session");
+        }
+        catch(Exception e) {
+            fail("Unhandled null pointer");
+        }
+    }
+
+    @Test
+    public void editValidSessionAtValidIndex() {
+        sessionList.editSession(0, s);
+        assertTrue(sessionList.getSession(0).equals(s), "Edit was successful");
+    }
+
+    @Test
+    public void editValidSessionAtInvalidIndex() {
+        assertFalse(sessionList.editSession(-1, s), "Should return false because index is out of bounds");
+    }
+
+    @Test
+    public void editNullSessionAtValidIndex() {
+        assertFalse(sessionList.editSession(0, null), "Should return false because session is null");
+    }
+
+    @Test
+    public void editNullSessionAtInvalidIndex() {
+        assertFalse(sessionList.editSession(-1, null), "Should return false because index is out of bounds and session is null");
+    }
+
+    @Test
+    public void removePresentSessionByDate() {
+        Date start = sessionList.getSession(0).getStartDate();
+        sessionList.removeSession(start);
+        assertTrue(sessionList.getSession(start) == null, "Should return null as session should be removed");
+    }
+
+    @Test
+    public void removeAbsentSessionByDate() {
+        assertFalse(sessionList.removeSession(s.getStartDate()), "Should be false as no session exists with that start date");
+    }
+
+    @Test
+    public void removeSessionByNullDate() {
+        assertFalse(sessionList.removeSession(null), "Should be false as no session has a null start date");
+    }
+
+    @Test
+    public void removeSessionAtValidIndex() {
+        Date start = sessionList.getSession(0).getStartDate();
+        sessionList.removeSession(0);
+        assertTrue(sessionList.getSession(start) == null, "Should return null as session should be removed");
+    }
+
+    @Test
+    public void removeSessionAtInvalidIndex() {
+        assertFalse(sessionList.removeSession(-551), "Should be false as this index is out of bounds");
+    }
+
+    @Test
+    public void getPresentCamperSessions() {
+        Camper c = createKBasicCampers(1)[0];
+        int count = 0;
+        for(Session session: sessionList.getAllSessions()) {
+            session.addCamper(c);
+            sessionList.editSession(session);
+            count++;
+        }
+        ArrayList<Session> cSessions = sessionList.getCamperSessions(c);
+        assertEquals(cSessions.size(), count,  "Camper is in count number of sessions"); 
+    }
+
+    @Test
+    public void getAbsentCamperSessions() {
+        Camper c = createKBasicCampers(1)[0];
+        assertTrue(sessionList.getCamperSessions(c).size() == 0, "Camper is not in any sessions");
+    }
+
+    @Test
+    public void getNullCamperSessions() {
+        assertTrue(sessionList.getCamperSessions(null).size() == 0, "Null campers are not in any sessions");
+    }
+
+    @Test
+    public void getPresentCounselorSessions() {
+       Counselor c = createBasicCounselor();
+       Session session = sessionList.getSession(0);
+       session.addCounselor(c);
+       sessionList.editSession(session);
+       ArrayList<Session> cSessions = sessionList.getCounselorSessions(c);
+       assertEquals(cSessions.size(),1, "Counselor is present in one session");
+    }
+
+    @Test
+    public void getAbsentCounselorSessions() {
+        Counselor c = createBasicCounselor();
+        assertEquals(sessionList.getCounselorSessions(c).size(), 0, "Counselor is not in any sessions");
+    }
+
+    @Test
+    public void getNullCounselorSessions() {
+        try {
+            assertEquals(sessionList.getCounselorSessions(null).size(), 0, "Null counselor is not in any session");
+        }
+        catch(Exception e) {
+            fail("Unhandled null pointer");
+        }
+        
     }
 }
